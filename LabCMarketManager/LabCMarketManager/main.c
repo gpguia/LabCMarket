f6734ea7e7baf245290a6ed25724b7df3e339fa5
Guilherme Guia <gpguia.pt@gmail.com>
2017-05-07 16:22:41 +0100

    Configuring to run in a linux ubuntu (virtual box)

--- LabCMarketManager/LabCMarketManager/main.c
+++ LabCMarketManager/LabCMarketManager/main.c
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[]) {
     int sock;
     
     system(execServ);
-    sleep(1);
+    sleep(1);//Sleep so the server can open and then the manager can connect to it.
     sock = connToServer();
     validManager(sock);
     return 0;
--- LabCMarketManager/LabCMarketManager/startServ.sh
+++ LabCMarketManager/LabCMarketManager/startServ.sh
@@ -1,1 +1,14 @@
-open -a Terminal "/Users/guilhermeguia/GoogleDrive/School/UP/2Semestre/LabDeComp/Project/LabCMarket/LabCMarketServ/LabCMarketServ/./serv"
+#!/bin/bash
+
+#pid= pidof "LabCServ"
+
+
+if pgrep -x "LabCServ" > /dev/null
+then
+		ps -ef | grep "LabCServ" | grep -v grep | awk '{print $2}' | xargs kill
+		cd /Users/guilhermeguia/GoogleDrive/School/UP/2Semestre/LabDeComp/Project/LabCMarket/LabCMarketServ/LabCMarketServ/ && gcc main.c LibServ.c -o LabCServ && chmod +x LabCServ
+		open -a Terminal "/Users/guilhermeguia/GoogleDrive/School/UP/2Semestre/LabDeComp/Project/LabCMarket/LabCMarketServ/LabCMarketServ/./LabCServ"
+else
+    cd /Users/guilhermeguia/GoogleDrive/School/UP/2Semestre/LabDeComp/Project/LabCMarket/LabCMarketServ/LabCMarketServ/ && gcc main.c LibServ.c -o LabCServ && chmod +x LabCServ
+		open -a Terminal "/Users/guilhermeguia/GoogleDrive/School/UP/2Semestre/LabDeComp/Project/LabCMarket/LabCMarketServ/LabCMarketServ/./LabCServ"
+fi
